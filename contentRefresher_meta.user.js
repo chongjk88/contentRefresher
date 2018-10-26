4d0784badf5ea6b5ed8a02d88d2646e1bd3bc199
Jun Kim <jun.kim@cdk.com>
2018-10-26 15:32:15 -0700

    download setup

--- contentRefresher_meta.user.js
+++ contentRefresher_meta.user.js
@@ -7,5 +7,7 @@
 // @include      https://websites.cobalt.com/wsm/editSite*
 // @include      http://websites.cobalt.com/wsm/seo_performance*
 // @include      https://websites.cobalt.com/wsm/seo_performance*
+// @downloadURL  https://github.com/chongjk88/contentRefresher/raw/master/contentRefresher_meta.user.js
+// @require      https://github.com/chongjk88/contentRefresher/raw/master/contentRefresher.js
 // @grant        none
 // ==/UserScript==
