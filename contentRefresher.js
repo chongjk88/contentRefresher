1956edbc5c3aa6ef4de69a02902322679afd7a1c
Jun Kim <jun.kim@cdk.com>
2018-10-29 12:26:56 -0700

    enhancement

--- contentRefresher.js
+++ contentRefresher.js
@@ -3,8 +3,6 @@ function now() {
   const refreshButton = document.createElement("div");
 
   wsmHeader.appendChild(refreshButton);
-  refreshButton.id = "testHi";
-
   refreshButton.textContent = "↻";
   refreshButton.style.display = "block";
   refreshButton.style.cursor = "pointer";
@@ -15,6 +13,8 @@ function now() {
   refreshButton.style.fontFamily = "-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'";
 
   if(window.location.href.indexOf("editSite.do") > -1) {
+      refreshButton.setAttribute("title", "Click to refresh current page");
+
       refreshButton.onclick = function() {
           const contentFrame = jQuery("iframe#cblt_content").contents();
           const siteEditorIframe = contentFrame.find("iframe#siteEditorIframe").contents();
@@ -32,12 +32,46 @@ function now() {
   }
 
   if(window.location.href.indexOf("seo_performance.mvc") > -1) {
-      refreshButton.onclick = function() {
-          const contentFrame = jQuery("iframe#siteToolView").contents();
-          const blogEditorIframe = contentFrame.find("iframe#blogEditorFrame").attr("src");
 
-          contentFrame.find("iframe#blogEditorFrame").attr("src", blogEditorIframe);
-      };
+      const seoFrame = jQuery("iframe#siteToolView").contents();
+      const blogTab = seoFrame.find("span:contains('Blog')");
+
+      if (blogTab.hasClass("ng-binding")) {
+
+          refreshButton.setAttribute("title", "Click to refresh Blog Creator");
+
+          const socialButton = document.createElement("div");
+          wsmHeader.appendChild(socialButton);
+          socialButton.setAttribute("title", "Click to open Blog Creator in a new window");
+          socialButton.textContent = "Open Blog Creator";
+          socialButton.style.display = "block";
+          socialButton.style.cursor = "pointer";
+          socialButton.style.position = "absolute";
+          socialButton.style.top = "30px";
+          socialButton.style.left = "50%";
+          socialButton.style.transform = "translateX(-50%)";
+          socialButton.style.fontSize = "12px";
+          socialButton.style.fontFamily = "-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'";
+
+          refreshButton.onclick = function() {
+
+              if (blogTab.parent().parent().hasClass("active")) {
+
+                  const contentFrame = jQuery("iframe#siteToolView").contents();
+                  const blogEditorIframe = contentFrame.find("iframe#blogEditorFrame").attr("src");
+
+                  contentFrame.find("iframe#blogEditorFrame").attr("src", blogEditorIframe);
+              } else {
+                  alert("This only works on the Blog tab");
+              }
+          };
+          socialButton.onclick = function() {
+              const getWebId = Cobalt.ModulesData["Cobalt.WSM.Common.SiteInfoModule"].siteMetaInfo[0].value;
+              window.open("https://reputationmanagement.cdk.com/tab/mvc/publisher/page?requestChannel=WSM&webId=" + getWebId + "&locale=null",'_blank');
+          };
+      } else {
+          refreshButton.style.display = "none";
+      }
   }
 }
 window.onload = now;
