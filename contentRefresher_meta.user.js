05ff21d60a10c81cab215514b8f9b2723accd0b3
Jun Kim <jun.kim@cdk.com>
2018-10-30 15:22:10 -0700

    update URLs

--- contentRefresher_meta.user.js
+++ contentRefresher_meta.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Content Refresher
-// @version      0.4
+// @version      0.5
 // @author       jk
 // @include      http://websites.cobalt.com/wsm/editSite.do?*
 // @include      https://websites.cobalt.com/wsm/editSite.do?*
-// @include      http://websites.cobalt.com/wsm/seo_performance.mvc*
-// @include      https://websites.cobalt.com/wsm/seo_performance.mvc*
+// @include      http://websites.cobalt.com/wsm/seo_performance.mvc?*
+// @include      https://websites.cobalt.com/wsm/seo_performance.mvc?*
 // @downloadURL  https://github.com/chongjk88/contentRefresher/raw/master/contentRefresher_meta.user.js
 // @require      https://github.com/chongjk88/contentRefresher/raw/master/contentRefresher.js
 // @grant        none
