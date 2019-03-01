c00e6936bdfcb07975f4382f60228d48a8db8e6f
Jun Kim <jun.kim@cdk.com>
2019-03-01 09:29:38 -0800

    Update Version

--- contentRefresher_meta.user.js
+++ contentRefresher_meta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Content Refresher
-// @version      0.6
+// @version      0.7
 // @author       jk
 // @include      */wsm/editSite.do*
 // @include      */wsm/seo_performance.mvc*
