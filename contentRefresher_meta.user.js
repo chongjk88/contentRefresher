e4ae49d3626667a8d03958720937aceee499d78e
Jun Kim <jun.kim@cdk.com>
2019-03-01 09:36:00 -0800

    Improved URL

--- contentRefresher_meta.user.js
+++ contentRefresher_meta.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Content Refresher
-// @version      0.7
+// @version      0.8
 // @author       jk
-// @include      */wsm/editSite.do*
-// @include      */wsm/seo_performance.mvc*
+// @include      *.com/wsm/editSite.do*
+// @include      *.com/wsm/seo_performance.mvc*
 // @downloadURL  https://github.com/chongjk88/contentRefresher/raw/master/contentRefresher_meta.user.js
 // @require      https://github.com/chongjk88/contentRefresher/raw/master/contentRefresher.js
 // @grant        none
