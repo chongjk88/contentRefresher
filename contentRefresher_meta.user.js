60ce895fbf9a951867ba6ebd6ec1116b3dd47cc0
Jun Kim <jun.kim@cdk.com>
2019-03-01 09:29:22 -0800

    Update URL

--- contentRefresher_meta.user.js
+++ contentRefresher_meta.user.js
@@ -2,10 +2,8 @@
 // @name         Content Refresher
 // @version      0.6
 // @author       jk
-// @include      http://websites.cobalt.com/wsm/editSite.do*
-// @include      https://websites.cobalt.com/wsm/editSite.do*
-// @include      http://websites.cobalt.com/wsm/seo_performance.mvc*
-// @include      https://websites.cobalt.com/wsm/seo_performance.mvc*
+// @include      */wsm/editSite.do*
+// @include      */wsm/seo_performance.mvc*
 // @downloadURL  https://github.com/chongjk88/contentRefresher/raw/master/contentRefresher_meta.user.js
 // @require      https://github.com/chongjk88/contentRefresher/raw/master/contentRefresher.js
 // @grant        none
